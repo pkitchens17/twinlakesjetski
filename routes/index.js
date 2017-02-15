8c78c1f90d847b7ef6cc665bb23d5071e55407a6
SourJello <patrick.kitchens1@gmail.com>
2017-02-14 23:06:37 -0500

    First commit

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+
+
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Twin Lakes Jet Ski' });
+});
+
+module.exports = router;
