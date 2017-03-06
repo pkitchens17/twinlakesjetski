6e3dbbafc61332e05bd9235240ad938aa741d3cd
SourJello <patrick.kitchens1@gmail.com>
2017-03-05 19:45:56 -0500

    began the process of implementing the mongodb

--- app.js
+++ app.js
@@ -6,7 +6,12 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
+var reservations = require('./routes/reservations');
+
+
+var configDB = require('./config/database.js');
+var db = mongoose.connect(configDB.url);
+
 
 var app = express();
 
@@ -30,7 +35,7 @@ app.get('/reservations', function(req, res) {
 })
 
 app.use('/', index);
-app.use('/users', users);
+app.use('/reservations', reservations);
 
 // these allow linking to the css pages and whatever else
 app.use('/js', express.static(__dirname + '/node_modules/materialize-css/dist/js'));
---
+++ routes/reservationsdb.js
@@ -0,0 +1,61 @@
+/*Auth: Patrick Kitchens
+* Page for connecting to the mongodb on mlab.com
+*Contains the reservation Schema
+*
+*
+*/
+var mongodb = require('mongodb');
+
+
+//creating the schema for the database
+var  reservationsSchema = mongoose.Schema({
+
+    reservations :{
+      boatID      : String,
+      startTime   : String,
+      endTime     : String,
+      startDate   : String,
+      endDate     : String,
+      tube        : String,
+      cost        : String,
+      firstName   : String,
+      lastName    : String,
+      phoneNumber : String,
+      email       : String
+
+    },
+
+    boats :{
+      boatID    : String,
+      boatType  : String,
+      location  : String
+    }
+
+});
+
+
+module.exports = mongoose.model('reservations', reservationsSchema);
+
+/*
+// Standard URI format: mongodb://[dbuser:dbpassword@]host:port/dbname
+var uri = 'mongodb://<twinlakesadmin>:<jetski>@ds153729.mlab.com:53729/twinlakesjetskidb';
+
+mongodb.MongoClient.connect(uri, function(err, db){
+
+    if(err) throw err;
+
+    //Lets add a reservation
+    var reservations = db.collection('reservations');
+
+    //insert method can take either an array or a dict
+    reservations.insert(seedData, function(err, result){
+
+            if(err) throw err;
+
+    });
+
+
+
+});
+
+*/
