23cf97c9d16e574b9eb14986dcde9b90aa55414d
SourJello <patrick.kitchens1@gmail.com>
2017-03-05 23:24:44 -0500

    deleted unecessary route in index.js

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,5 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Twin Lakes Jet Ski' });
 });
 
-router.get('/prices', function(req, res, next) {
-  res.render('prices', { title: 'Twin Lakes Jet Ski' });
-});
+
 module.exports = router;
