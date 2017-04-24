76fdc9dfc12183795cf5b8d04abe282db66c6a3b
Patrick GCSU <patrick.kitchens@bobcats.gcsu.edu>
2017-04-24 14:07:15 -0400

    implemented the get method in angular and displaying the data on the admin page

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -12,16 +12,32 @@ var refresh =function(){
   .then(function(response) {
     console.log("I got the data requested");
     console.log(response.data);
-  $scope.reservationslist = response.data;
+  $scope.reservationslist = [];
+  response.data.forEach(function(data){
+    $scope.reservationslist.push(data.reservations);
+
+  });
+  //alert($scope.reservationslist.toString());
+
+  $scope.reservation= [];
   //alert($scope.reservation.lastName);
   //  alert(reservation.lastName);
     //alert($scope.data);
-    $scope.temp = angular.fromJson($scope.reservationslist)
-    alert($scope.temp);
-    console.log($scope.temp.reservations.firstName);
+  //  $scope.temp = angular.fromJson($scope.reservationslist)
+  //  alert($scope.temp);
+    //console.log($scope.temp.reservations.firstName);
+
+
+/*
+    angular.forEach($scope.reservationslist.data, function(value, key){
+      this.push(key + value);
+    }, log);
     });
 
-  };
+    */
+
+  });
+};
 
 refresh();
 
