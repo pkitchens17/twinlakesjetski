61f98076e3e39a86d955558fc2f826e2000a018d
Patrick GCSU <patrick.kitchens@bobcats.gcsu.edu>
2017-04-27 22:25:26 -0400

    added form validation on fields in reservation

--- routes/reservationsdb.js
+++ routes/reservationsdb.js
@@ -22,9 +22,7 @@ var  reservationsSchema = mongoose.Schema({
       lastName    : { type: String, lowercase:true},
       phoneNumber : String,
       email       :  { type: String, lowercase:true},
-      location    :  { type: String, lowercase:true},
-      boatID      :  { type: String, lowercase:true}
-
+      location    :  { type: String, lowercase:true}
     },
 
 });
